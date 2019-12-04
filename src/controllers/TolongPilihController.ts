3fb102816e373d273b0c578482ef107c12350482
MNFiT3 <mnfitv@gmail.com>
2019-12-04 20:18:17 +0800

    #1
    add dummy function

---
+++ src/controllers/TolongPilihController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+import { getRepository } from "typeorm";
+import { validate } from "class-validator";
+
+import config from "../config/config";
+
+export class TolongPilihController {
+    static joinGroup = async (req: Request, res: Response) => {
+
+    }
+
+    static leaveGroup = async (req: Request, res: Response) => {
+
+    }
+
+    static inviteGroup = async (req: Request, res: Response) => {
+
+    }
+
+    static addItems = async (req: Request, res: Response) => {
+
+    }
+
+    static removeItems = async (req: Request, res: Response) => {
+
+    }
+
+    static tolongPilih = async (req: Request, res: Response) => {
+
+    }
+
+}
