9f75d27ec4e233e91db2efc2296ef25878e3e152
MNFiT3 <mnfitv@gmail.com>
2019-12-04 20:47:36 +0800

    #4
    - Try to make some process modular

--- src/controllers/TolongPilihController.ts
+++ src/controllers/TolongPilihController.ts
@@ -8,12 +8,68 @@ import { UserGroup } from "../entity/UserGroup";
 import { Group } from "../entity/Group";
 import { User } from "../entity/User";
 
-export class TolongPilihController {
+export default class TolongPilihController {
+
+    static checkUserAndGroup = async (groupId: any, userId: any, res: Response, callback: Function) => {
+        if (!groupId) {
+            res.send('Missing attibute groupId')
+            callback(true)
+        }
+
+        let user: User
+        try {
+            user = await getRepository(User).findOneOrFail(userId)
+        } catch (error) {
+            res.status(401).send()
+            callback(true)
+        }
+
+        let theGroup: Group
+        try {
+            theGroup = await getRepository(Group).findOneOrFail(groupId)
+        } catch (error) {
+            res.status(404).send('Group not found')
+            callback(true)
+        }
+
+        callback(false, user, theGroup)
+    }
+
     static joinGroup = async (req: Request, res: Response) => {
         const { groupId } = req.body
         const userId = res.locals.jwtPayload.userId
 
-        if(!groupId){
+
+        TolongPilihController.checkUserAndGroup(groupId, userId, res, async (err, user, theGroup) => {
+            if (!err) {
+                let newUserGroup = new UserGroup()
+                newUserGroup.group = theGroup
+                newUserGroup.user = user
+
+                const errors = await validate(newUserGroup);
+                if (errors.length > 0) {
+                    res.status(400).send(errors);
+                    return;
+                }
+
+                const userGroupRepo = getRepository(UserGroup)
+                try {
+                    await userGroupRepo.save(newUserGroup)
+                } catch (error) {
+                    res.status(409).send()
+                    return
+                }
+
+                res.status(204).send()
+            }
+        })
+    }
+
+    static leaveGroup = async (req: Request, res: Response) => {
+        const { groupId } = req.body
+        const userId = res.locals.jwtPayload.userId
+
+        if (!groupId) {
             res.send('Missing attibute groupId')
             return
         }
@@ -23,7 +79,7 @@ export class TolongPilihController {
             user = await getRepository(User).findOneOrFail(userId)
         } catch (error) {
             res.status(401).send()
-            return   
+            return
         }
 
         let theGroup: Group
@@ -33,30 +89,6 @@ export class TolongPilihController {
             res.status(404).send('Group not found')
             return
         }
- 
-        let newUserGroup = new UserGroup()
-        newUserGroup.group = theGroup
-        newUserGroup.user = user
-
-        const errors = await validate(newUserGroup);
-        if (errors.length > 0) {
-          res.status(400).send(errors);
-          return;
-        }
-
-        const userGroupRepo = getRepository(UserGroup)
-        try {
-            await userGroupRepo.save(newUserGroup)
-        } catch (error) {
-            res.status(409).send()
-            return
-        }
-
-        res.status(204).send()
-    }
-
-    static leaveGroup = async (req: Request, res: Response) => {
-
     }
 
     static inviteGroup = async (req: Request, res: Response) => {
