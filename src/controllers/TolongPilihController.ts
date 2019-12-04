ef82bdd874f2b17db639c34231d4c1af25524dae
MNFiT3 <mnfitv@gmail.com>
2019-12-04 20:30:59 +0800

    #2
    - Added join group API
    - Not tested

--- src/controllers/TolongPilihController.ts
+++ src/controllers/TolongPilihController.ts
@@ -4,10 +4,55 @@ import { getRepository } from "typeorm";
 import { validate } from "class-validator";
 
 import config from "../config/config";
+import { UserGroup } from "../entity/UserGroup";
+import { Group } from "../entity/Group";
+import { User } from "../entity/User";
 
 export class TolongPilihController {
     static joinGroup = async (req: Request, res: Response) => {
+        const { groupId } = req.body
+        const userId = res.locals.jwtPayload.userId
 
+        if(!groupId){
+            res.send('Missing attibute groupId')
+            return
+        }
+
+        let user: User
+        try {
+            user = await getRepository(User).findOneOrFail(userId)
+        } catch (error) {
+            res.status(401).send()
+            return   
+        }
+
+        let theGroup: Group
+        try {
+            theGroup = await getRepository(Group).findOneOrFail(groupId)
+        } catch (error) {
+            res.status(404).send('Group not found')
+            return
+        }
+ 
+        let newUserGroup = new UserGroup()
+        newUserGroup.group = theGroup
+        newUserGroup.user = user
+
+        const errors = await validate(newUserGroup);
+        if (errors.length > 0) {
+          res.status(400).send(errors);
+          return;
+        }
+
+        const userGroupRepo = getRepository(UserGroup)
+        try {
+            await userGroupRepo.save(newUserGroup)
+        } catch (error) {
+            res.status(409).send()
+            return
+        }
+
+        res.status(204).send()
     }
 
     static leaveGroup = async (req: Request, res: Response) => {
