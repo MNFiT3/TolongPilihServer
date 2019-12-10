77e73935dab33a31017cfbf509d053ca2a63bae9
MNFiT3 <mnfitv@gmail.com>
2019-12-11 02:50:01 +0800

    Added public folder

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import * as bodyParser from "body-parser";
 import * as helmet from "helmet";
 import * as cors from "cors";
 import routes from "./routes";
+import * as path from "path";
 
 const PORT = 3001 || process.env.PORT
+const webURL = "web";
 
 //Connects to the Database -> then starts the express
 createConnection()
@@ -19,6 +21,17 @@ createConnection()
     app.use(helmet());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+
+    app.get("/", (req, res) => {
+      var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+      res.status(301).redirect(fullUrl + webURL)
+    })
+
+    if(process.env.NODE_ENV.trim() === "production"){
+      app.use('/' + webURL, express.static(path.join(__dirname, '../', 'src', 'public')));
+    }else{
+      app.use('/' + webURL, express.static(path.join(__dirname, 'public')));
+    }
 
     //Set all routes from routes folder
     app.use("/api", routes);
